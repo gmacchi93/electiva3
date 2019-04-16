f31537a958ff655551e159f20b3ec51bc5f5342f
gmacchi <gmacchi@bellbird.com.py>
2019-04-15 23:27:41 -0400

    - jquery

--- js/todolist.js
+++ js/todolist.js
@@ -5,10 +5,10 @@
  * @since 04/03/19.
  * @version 1.0
  */
-(() => {
+(($) => {
     'use strict';
 
-    const API_URL = 'https://task-backend-fpuna.herokuapp.com/tasks';
+    const API_URL = 'http://task-backend-fpuna.herokuapp.com/tasks';
     const TASK_STATUS = {
         PENDING: 'PENDIENTE',
         DONE: 'TERMINADO',
@@ -28,9 +28,14 @@
      * This method is executed once the page have just been loaded and call the service to retrieve the
      * list of tasks
      */
-    document.onreadystatechange = () => {
-        Ajax.sendGetRequest(API_URL, null, MediaFormat.JSON, (value) => loadTasks(value), (code) => showError(code, 'La tarea no ha podido ser añadida.'), true);
-    };
+    $(document).on('readystatechange', () => {
+        $.get(API_URL).done(function (data) {
+            loadTasks(data);
+        }).fail((code) => {
+            showError(code, 'La tarea no ha podido ser añadida.');
+        });
+        // Ajax.sendGetRequest(API_URL, null, MediaFormat.JSON, (value) => loadTasks(value), (code) => showError(code, 'La tarea no ha podido ser añadida.'), true);
+    });
 
     /**
      * This method displays an error on the page.
@@ -39,11 +44,15 @@
      */
     const showError = (code, text) => {
         console.log(text);
-        let errorBar = document.getElementsByClassName("error-bar")[0];
-        errorBar.innerText = text;
-        errorBar.classList.remove("hide-bar");
+        let errorBar = $(".error-bar");
+        // let errorBar = document.getElementsByClassName("error-bar")[0];
+        errorBar.text = text;
+        // errorBar.innerText = text;
+        errorBar.removeClass("hide-bar");
+        // errorBar.classList.remove("hide-bar");
         setTimeout(function () {
-            errorBar.classList.add("hide-bar");
+            errorBar.addClass('hide-bar');
+            // errorBar.classList.add("hide-bar");
         }, 5000);
 
     };
@@ -54,9 +63,7 @@
      *
      * @param array the string coming on the body of the API response
      */
-    const loadTasks = (array) => {
-
-        let tasks = JSON.parse(array);
+    const loadTasks = (tasks) => {
         for (let i in tasks) {
             if (tasks.hasOwnProperty(i)) {
                 addTaskToList(tasks[i]);
@@ -72,16 +79,35 @@
      */
     const addTask = (e) => {
 
-        let newTaskInput = document.getElementById("new-task");
-        let content = newTaskInput.value;
+        let newTaskInput = $("#new-task");
+        // let newTaskInput = document.getElementById("new-task");
+        let content = newTaskInput.val();
         if (content.length === 0) return false;
 
         e.preventDefault();
 
         let task = new Task(content);
-        Ajax.sendPostRequest(API_URL, task, MediaFormat.JSON, (value) => addTaskToList(JSON.parse(value)), (code) => showError(code, 'La tarea no ha podido ser añadida.'), true);
+        $.ajax({
+            method: "POST",
+            url: API_URL,
+            data: JSON.stringify(task),
+            contentType: 'application/json'
+        }).done((value) => {
+            addTaskToList(value);
+        }).fail((code) => {
+            console.log(code);
+            showError(code, 'La tarea no ha podido ser añadida.');
+        });
 
-        newTaskInput.value = "";
+        // $.post(API_URL, JSON.stringify(task))
+        //     .done((value) => {
+        //     addTaskToList(JSON.parse(value));
+        // }).fail((code) => {
+        //     showError(code, 'La tarea no ha podido ser añadida.');
+        // });
+        // Ajax.sendPostRequest(API_URL, task, MediaFormat.JSON, (value) => addTaskToList(JSON.parse(value)), (code) => showError(code, 'La tarea no ha podido ser añadida.'), true);
+
+        newTaskInput.val("");
 
 
         return false;
@@ -90,28 +116,38 @@
     /**
      * This procedure links the new task button the addTask method on the click event.
      */
-    let addButtons = document.getElementsByClassName('add');
-    for (let i in addButtons)
-        addButtons.item(Number(i)).onclick = (e) => addTask(e);
+    let addButtons = $('.add');
+    addButtons.click((e) => {
+        addTask(e);
+    });
 
     /**
      * We associate a function to manipulate the DOM once the checkbox value is changed.
      * Change the task to the completed or incomplete list (according to the status)
      */
     const addOnChangeEvent = (task) => {
-        const checkBox = document.getElementById(`task-${task.id}`).querySelector('label > input');
-        checkBox.onchange = (e) => {
-            if (e.target.checked) {
+        const checkBox = $('#task-' + task.id).find('label').find('input');
+
+        checkBox.click(function () {
+            console.log($(this).prop("checked"));
+            if ($(this).prop("checked")) {
                 task.status = TASK_STATUS.DONE;
-            } else {
+            }else{
                 task.status = TASK_STATUS.PENDING;
             }
-            document.getElementById(`task-${task.id}`).remove();
-            Ajax.sendPutRequest(API_URL + "/" + task.id, task, MediaFormat.JSON,
-                (value) => addTaskToList(JSON.parse(value)),
-                (code, value) => showError(code, 'La tarea no ha podido ser actualizada.'),
-                true);
-        };
+            $('#task-' + task.id).remove();
+            $.ajax({
+                method: "PUT",
+                url: API_URL + "/" + task.id,
+                data: JSON.stringify(task),
+                contentType: 'application/json'
+            }).done((value) => {
+                addTaskToList(value);
+
+            }).fail((code) => {
+                showError(code, 'La tarea no ha podido ser actualizada.');
+            });
+        });
     };
 
     /**
@@ -119,48 +155,50 @@
      * @param task the new task.
      */
     const addTaskToList = (task) => {
-        let newItem = document.createElement('li');
-        newItem.setAttribute('id', `task-${task.id}`);
+        let newItem = $('<li id="task-' + task.id + '"></li>');
 
-
-        let label = document.createElement('label');
+        let label = $('<label></label>');
         if (task.status !== TASK_STATUS.CANCEL) {
-            label.innerHTML = `<input type="checkbox" ${task.status === TASK_STATUS.DONE ? "checked" : ""}/> ${task.description}`;
+            let checked = "";
+            if (task.status === TASK_STATUS.DONE){
+                checked = "checked";
+            }
+            label.html('<input type="checkbox" ' + checked+' />'+ task.description);
         } else {
-            label.innerHTML = `${task.description}`;
+            label.html(task.description);
         }
-        let editButton = document.createElement('button');
-        editButton.innerText = 'Editar';
-        editButton.classList.add('edit');
-        editButton.setAttribute('data-id', task.id);
-        editButton.onclick = (e) => editTask(e);
+        let textEditButton = "<button class='edit' data-id='" + task.id + "'>Editar</button>";
+        let editButton = $(textEditButton);
+        editButton.click((e) => {
+            editTask(e);
+        });
 
-        let deleteButton = document.createElement('button');
-        deleteButton.innerText = 'Borrar';
-        deleteButton.classList.add('delete');
-        deleteButton.setAttribute('data-id', task.id);
-        deleteButton.onclick = (e) => removeTask(e);
-        newItem.appendChild(label);
-        newItem.appendChild(editButton);
-        newItem.appendChild(deleteButton);
-        console.log(task.status);
+        let textDeleteButton = "<button class='delete' data-id='" + task.id + "'>Borrar</button>";
+        let deleteButton = $(textDeleteButton);
+        deleteButton.click((e) => {
+            removeTask(e);
+        });
+
+        newItem.append(label);
+        newItem.append(editButton);
+        newItem.append(deleteButton);
 
         if (task.status === TASK_STATUS.PENDING) {
-            let cancelButton = document.createElement('button');
-            cancelButton.innerText = 'Cancelar';
-            cancelButton.classList.add('cancel');
-            cancelButton.setAttribute('data-id', task.id);
-            cancelButton.onclick = (e) => cancelTask(e);
-            newItem.appendChild(cancelButton);
+            let textCancelButton = "<button class='cancel' data-id='" + task.id + "'>Cancelar</button>";
+            let cancelButton = $(textCancelButton);
+            cancelButton.click((e) => {
+                cancelTask(e);
+            });
+            newItem.append(cancelButton);
         }
 
 
         if (task.status === TASK_STATUS.PENDING)
-            document.getElementById('incomplete-tasks').appendChild(newItem);
+            $('#incomplete-tasks').append(newItem);
         else if (task.status === TASK_STATUS.DONE)
-            document.getElementById('completed-tasks').appendChild(newItem);
+            $('#completed-tasks').append(newItem);
         else
-            document.getElementById('canceled-tasks').appendChild(newItem);
+            $('#canceled-tasks').append(newItem);
         if (task.status !== TASK_STATUS.CANCEL) {
             addOnChangeEvent(task);
         }
@@ -176,44 +214,58 @@
         // We retrieve the value of the attribute data-id;
         const id = e.target.dataset.id;
 
-        let currentDOMTask = document.getElementById(`task-${id}`);
-        currentDOMTask.querySelector("label > input[type=checkbox]").remove();
+        let currentDOMTask = $('#task-' + id);
+        currentDOMTask.find("input[type=checkbox]").remove();
+        // let currentDOMTask = document.getElementById(`task-${id}`);
+        // currentDOMTask.querySelector("label > input[type=checkbox]").remove();
 
-        let currentTask = new Task(currentDOMTask.querySelector("label").innerHTML.trim());
+        let currentTask = new Task(currentDOMTask.find("label").text().trim());
         currentTask.id = id;
+        // let currentTask = new Task(currentDOMTask.querySelector("label").innerHTML.trim());
+        // currentTask.id = id;
 
-        currentDOMTask.querySelector('label').remove();
+        currentDOMTask.find('label').remove();
 
-        let inputText = document.createElement('input');
-        inputText.setAttribute('id', `task-edit-${currentTask.id}`);
-        inputText.setAttribute('type', 'text');
-        inputText.setAttribute('value', currentTask.description);
+        let inputText = $("<input id='task-edit-" + id + "' type='text' value='" + currentTask.description + "' />");
+
 
         /**
          * We associate the event click on the button ok, to send a PUT request to the server.
          */
-        let buttonOK = document.createElement('button');
-        buttonOK.innerText = 'OK';
-        buttonOK.setAttribute('id', `ok-button-${currentTask.id}`);
-        buttonOK.onclick = () => {
-            currentTask.description = document.getElementById(`task-edit-${currentTask.id}`).value;
-            Ajax.sendPutRequest(API_URL + "/" + currentTask.id, currentTask, MediaFormat.JSON,
-                (value) => revertHTMLChangeOnEdit(value),
-                (code, value) => showError(code, 'La tarea no ha podido ser actualizada.'),
-                true);
-        };
 
-        let buttonCancel = document.createElement('button');
-        buttonCancel.innerText = 'Cancel';
-        buttonCancel.setAttribute('id', `cancel-button-${currentTask.id}`);
-        buttonCancel.onclick = () => revertHTMLChangeOnEdit(currentTask);
 
-        currentDOMTask.insertBefore(buttonCancel, currentDOMTask.children[0]);
-        currentDOMTask.insertBefore(buttonOK, currentDOMTask.children[0]);
-        currentDOMTask.insertBefore(inputText, currentDOMTask.children[0]);
+        let buttonOK = $('<button id="ok-button-' + id + '">OK</button>');
+        buttonOK.click((e) => {
+            currentTask.description = $('#task-edit-' + id).val();
+            $.ajax({
+                method: "PUT",
+                url: API_URL + "/" + currentTask.id,
+                data: JSON.stringify(currentTask),
+                contentType: 'application/json'
+            }).done((value) => {
+                revertHTMLChangeOnEdit(value);
+            }).fail((code) => {
+                showError(code, 'La tarea no ha podido ser actualizada.');
+            });
 
-        currentDOMTask.querySelector('.edit').style.visibility = 'hidden';
-        currentDOMTask.querySelector('.delete').style.visibility = 'hidden';
+        });
+
+
+        let buttonCancel = $('<button id="cancel-button-' + id + '">Cancel</button>');
+        buttonCancel.click(function () {
+            revertHTMLChangeOnEdit(currentTask)
+        });
+        currentDOMTask.prepend(buttonCancel);
+        currentDOMTask.prepend(buttonOK);
+        currentDOMTask.prepend(inputText);
+
+
+
+
+
+        currentDOMTask.find('.edit').css("visibility", 'hidden');
+        currentDOMTask.find('.delete').css("visibility", 'hidden');
+        currentDOMTask.find('.cancel').css("visibility", 'hidden');
 
         inputText.focus();
     };
@@ -223,23 +275,20 @@
      * @param currentTask the string coming from the API
      */
     const revertHTMLChangeOnEdit = (currentTask) => {
-        let task = JSON.parse(currentTask);
+        let task = currentTask;
+        let currentDOMTask = $('#task-' + task.id);
+        $('#task-' + task.id).find('input[type=text]').remove();
+        currentDOMTask.find('input[type=text]').remove();
 
-        let currentDOMTask = document.getElementById(`task-${task.id}`);
-        currentDOMTask.querySelector('input[type=text]').remove();
-
-        let label = document.createElement('label');
-
-        currentDOMTask.insertBefore(label, currentDOMTask.children[0]);
-        label.innerHTML = `<input type="checkbox"/> ${task.description}`;
-        addOnChangeEvent(task);
-
-        currentDOMTask.insertBefore(label, currentDOMTask.children[0]);
-        currentDOMTask.querySelector(`button#ok-button-${task.id}`).remove();
-        currentDOMTask.querySelector(`button#cancel-button-${task.id}`).remove();
-
-        currentDOMTask.querySelector('.edit').style.visibility = 'visible';
-        currentDOMTask.querySelector('.delete').style.visibility = 'visible';
+        let label = $("<label></label>");
+        currentDOMTask.prepend(label);
+        label.html("<input type='checkbox'/>" + task.description);
+        currentDOMTask.prepend(label);
+        currentDOMTask.find('button#ok-button-'+task.id).remove();
+        currentDOMTask.find('button#cancel-button-'+task.id).remove();
+        currentDOMTask.find('.edit').css('visibility', 'visible');
+        currentDOMTask.find('.delete').css('visibility', 'visible');
+        currentDOMTask.find('.cancel').css('visibility', 'visible');
     };
 
     /**
@@ -247,7 +296,8 @@
      * @param id the identifier from the task
      */
     const removeTaskFromList = (id) => {
-        document.getElementById(`task-${id}`).remove();
+        $('#task-' + id).remove();
+        // document.getElementById(`task-${id}`).remove();
     };
 
     /**
@@ -256,10 +306,14 @@
      */
     const removeTask = (e) => {
         const id = e.target.dataset.id;
-        Ajax.sendDeleteRequest(API_URL + '/' + id, null, MediaFormat.JSON,
-            (value) => removeTaskFromList(id),
-            (code) => showError(code, 'La tarea no ha podido ser eliminada.'),
-            true);
+        $.ajax({
+            method: "DELETE",
+            url: API_URL + '/' + id
+        }).done((value) => {
+            removeTaskFromList(id);
+        }).fail((code) => {
+            showError(code, 'La tarea no ha podido ser añadida.');
+        });
     };
 
     /**
@@ -270,12 +324,19 @@
         const id = e.target.dataset.id;
         const checkBox = document.getElementById(`task-${id}`).querySelector('label > input');
         if (!checkBox.checked) {
-            document.getElementById(`task-${id}`).remove();
-            Ajax.sendPutRequest(API_URL + "/" + id, {"status": TASK_STATUS.CANCEL}, MediaFormat.JSON,
-                (value) => addTaskToList(JSON.parse(value)),
-                (code, value) => showError(code, 'La tarea no ha podido ser cancelada.'),
-                true);
+
+            $.ajax({
+                method: "PUT",
+                url: API_URL + "/" + id,
+                data: JSON.stringify({"status": TASK_STATUS.CANCEL}),
+                contentType: 'application/json'
+            }).done((value) => {
+                $('#task-' + id).remove();
+                addTaskToList(value);
+            }).fail((code) => {
+                showError(code, 'La tarea no ha podido ser añadida.');
+            });
         }
     };
 })
-();
+(jQuery);
